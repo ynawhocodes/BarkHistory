8d37f5c01a4417c098e2b1b61eb7b3626183b09d
nawon.yang <nawon.yang@musinsa.com>
2021-11-18 18:01:30 +0900

    #1 글확인 프론트 구현

---
+++ swiper.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    centeredSlides: true,
+    loop: true,
+
+});
+
+var swiper = new Swiper(".mySwiper-reaction", {
+    slidesPerView: 8,
+    spaceBetween: 25,
+    loop: true,
+
+});
