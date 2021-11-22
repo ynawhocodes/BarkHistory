26812ee7b4899e8c4fe731848481ec7a834f16d0
ynawhocodes <ynawhocodes@gmail.com>
2021-11-22 16:10:26 +0900

    #1 reaction UI 구현 완성

---
+++ js/reaction.js
@@ -0,0 +1,8 @@
+var modal = document.getElementById('modal');
+
+function onClickPlus() {
+    modal.classList.add('modal-open');
+}
+function onClickClose() {
+    modal.classList.remove('modal-open');
+}
