48260f83dc9970d3cefc6c51498527815f95190d
icechocola05 <icechocola05@gmail.com>
2021-12-01 20:51:48 +0900

    #6 회원 가입, 로그인 백엔드 구현
    -아직 로그인 검증 확실하게 완료되지 않음
    -프론트와 합쳐지지 않음

---
+++ main.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+var db = require('./lib/db.js');
+var member = require('./lib/member.js');
+
+var app = http.createServer(function(request, response) {
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/') {
+        member.signIn(request, response);
+    } 
+    else if(pathname === '/member/signIn') {
+        member.signIn(request, response);
+    } else if(pathname === '/member/signUp') {
+        member.signUp(request, response);
+    } else if(pathname === '/member/signIn_process') {
+        member.signIn_process(request, response);
+    } else if(pathname === '/member/signUp_process') {
+        member.signUp_process(request, response);
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
+});
+app.listen(3000);
