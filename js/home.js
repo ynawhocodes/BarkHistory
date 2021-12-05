4f52cac95db20ead8136915fed9f5bafabdae187
Jihye_Bae <90167432+bjh0507@users.noreply.github.com>
2021-12-06 01:13:59 +0900

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,16 @@
+
+  var mySwiper = new Swiper('.mySwiper', {
+
+            slidesPerView: 7,
+             spaceBetween: 20,
+             centeredSlides: true,
+             mousewheel : true,
+
+    });
+
+
+  var swiper = new Swiper(".mySwiper-reaction", {
+      slidesPerView: 6,
+      spaceBetween: 25,
+      loop: true,
+  });
