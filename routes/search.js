db283b993776eaf9215039948174b0c4e1fbce1f
Jihye_Bae <90167432+bjh0507@users.noreply.github.com>
2021-12-19 23:37:43 +0900

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 const { sequelize } = require('../models');
@@ -13,8 +14,8 @@ router.get('/', function(request, response) {
 });
 
 
-router.get('/:searchString', async function(request, response, next) {
-    console.log('a');
+router.get('detail/searchString', async function(request, response, next) {
+    console.log('b');
     let list_1=[];
     try{
     const searchString = request.params.searchString;
@@ -29,18 +30,26 @@ router.get('/:searchString', async function(request, response, next) {
     response.render('search',{'list_1':list_1});
 });
 
-router.post('/', async(request, response, next) => {
-    try{
-        console.log('a');
+router.post('/d', async(request, response, next) => {
+    
+        console.log('c');
         const { searchString } = request.body;
         console.log(searchString);
         let result, metadata;
-        request.session.message = result;
+       // request.session.message = result;
+       let list_1=[];
+    try{
+    [result, meta] = await sequelize.query(`select post_title, post_detail, post_detail from post where post_title like '%${searchString}%' or post_detail like '%${searchString}%'`);
+    list_1.push(result[0]);
+    console.log(result);
+ }
+    catch {
+
     }
-    catch(err){
-        console.log(err);
-    }
-    res.redirect(`/search/${searchString}`);
+    console.log(list_1);
+    //list_1에 다 있습니다. 가져가서 render로 쓰시면 됩니다!   
+    //res.render('검색내용 표시할 .ejs파일명 ', {'ejs파일에서 쓸 변수명 ' : list_1});
+    //res.redirect(`/search/${searchString}`);
 
 });
 
