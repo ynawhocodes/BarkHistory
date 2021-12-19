46aadcf82db18e473ed9d20b41ee4fb90463d427
yanwhocodes <ynawhocodes@gmail.com>
2021-12-20 03:43:15 +0900

    #4 게시판 상세 1차 정리 - 반응에 따른 댓글 배경색 변경 미완 - 댓글 배경 초기화 스타일 변경 - 반응 클릭 후 댓글 생성 페이지로 이동 완료

--- js/reaction.js
+++ js/reaction.js
@@ -6,3 +6,39 @@ function onClickPlus() {
 function onClickClose() {
     modal.classList.remove('modal-open');
 }
+BgColor = {
+    emoji01: '#ff5c5b',
+    emoji02: '#68c0fe',
+    emoji03: '#faff0',
+    emoji04: '#00bb07',
+    emoji05: '#5a76fe',
+    emoji06: '#b301b7'
+};
+
+function setBgColor(id) {
+    console.log('psetBgColor');
+    console.log(id);
+    if(id === 'emoji01') {
+        document.getElementById('comment-box__setBgColor').style.backgroundColor = BgColor.emoji01;
+    }
+    else if(id === 'emoji02') {
+        document.getElementById('comment-box__setBgColor').style.backgroundColor = BgColor.emoji02;
+    }
+    else if(id === 'emoji03') {
+        document.getElementById('comment-box__setBgColor').style.backgroundColor = BgColor.emoji03;
+    }
+    else if(id === 'emoji04') {
+        document.getElementById('comment-box__setBgColor').style.backgroundColor = BgColor.emoji04;
+    }
+    else if(id === 'emoji05') {
+        document.getElementById('comment-box__setBgColor').style.backgroundColor = BgColor.emoji05;
+    }
+    else if(id === 'emoji06') {
+        document.getElementById('comment-box__setBgColor').style.backgroundColor = BgColor.emoji06;
+    }
+}
+function putEmoji(clickedID) {
+    console.log('putEmoji');
+    console.log('>>' + clickedID);
+    setBgColor(clickedID);
+}
