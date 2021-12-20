144204ba0868c83e1419a995f3f4c8dfc2ed53ef
jhsong <80883709+jhsong76@users.noreply.github.com>
2021-12-20 12:02:44 +0900

    #9 유저 확인 수정

--- routes/board.js
+++ routes/board.js
@@ -6,7 +6,6 @@ var auth = require('../lib/auth');
 var req = require('express/lib/request');
 var res = require('express/lib/response');
 
-
 // 글 작성
 router.get('/create', function (req, res) {
     db.query('SELECT * FROM category',
@@ -25,9 +24,13 @@ router.get('/create', function (req, res) {
 router.post('/create_process', function (req, res) {
     db.query('SELECT * FROM post where category_id=?',
         [req.params.category_id],
-        function (error, ) {
+        function (error, result) {
             if (auth.isOwner(req, res) === false) {
-                res.end('Login required!!');
+                // 로그인 페이지 이동
+                res.writeHead(302, {
+                    Location: `/auth/signIn`
+                });
+                res.end();
                 return false;
             }
             var post = req.body;
@@ -50,43 +53,39 @@ router.post('/create_process', function (req, res) {
 
 //글 목록 
 router.get('/:category_id', (req, res) => {
-    db.query("SELECT category_id from category", function (error, category_value) {
-        if (category_value[i].category_id == 1)
-            db.query(
-                "select category_name, category_detail from category left join post on category.category_id=post.category_id where category.category_id=? limit 1",
-                [req.params.category_id],
-                function (error, data) {
-                    if (error) {
-                        console.log(error);
-                    }
-                    db.query(
-                        "select post_title, post_detail, user_name, date_format(post_date, '%m/%d') as post_date, post_emotion_number from post left join user on post.user_id=user.user_id where category_id=? order by post_date desc",
-                        [req.params.category_id],
-                        function (error2, results) {
-                            if (error2) {
-                                throw error2;
-                            }
-                            db.query(
-                                'select post_title, post_detail, post_date from post where post_date between date_add(now(), interval -1 week)and now() and category_id=? order by post_emotion_number desc limit 1',
-                                [req.params.category_id],
-                                function (error3, result2) {
-                                    if (error3) {
-                                        console.log(error);
-                                    } else {
-                                        console.log(results);
-                                        res.render('post_list.html', {
-                                            boardDB_results: results,
-                                            hot_post: result2,
-                                            cate: data,
-                                            category: category_value
-                                        })
-
-                                    }
-
-                                })
-                        })
-                })
+        db.query( //카테고리 이름, 설명 가져오기
+            "select category_name, category_detail from category left join post on category.category_id=post.category_id where category.category_id=? limit 1",
+            [req.params.category_id],
+            function (error, data) {
+                if (error) {
+                    console.log(error);
+                }
+                db.query( //글 목록 가져오기
+                    "select post_title, post_detail, user_name, date_format(post_date, '%m/%d') as post_date, post_emotion_number from post left join user on post.user_id=user.user_id where category_id=? order by post_date desc",
+                    [req.params.category_id],
+                    function (error2, results) {
+                        if (error2) {
+                            throw error2;
+                        }
+                        db.query( //실시간 인기글 가져오기 
+                            'select post_title, post_detail, post_date from post where post_date between date_add(now(), interval -1 week)and now() and category_id=? order by post_emotion_number desc limit 1',
+                            [req.params.category_id],
+                            function (error3, result2) {
+                                if (error3) {
+                                    console.log(error);
+                                } else {
+                                    console.log(results);
+                                    res.render('post_list.html', {
+                                        //글 목록
+                                        boardDB_results: results,
+                                        //실시간 인기글
+                                        hot_post: result2,
+                                        //카테고리 정보 
+                                        cate: data,
+                                    })
+                                }
+                            })
+                    })
+            })
     })
-})
-
 module.exports = router;
