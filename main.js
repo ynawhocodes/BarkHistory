09ea88beaf9e8cdbf8cfef23406f6e84b672a09e
jhsong <wngml203@naver.com>
2021-12-20 14:27:06 +0900

    #9 board server 추가

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-var member = require('./lib/member.js');
+/*var member = require('./lib/member.js');*/
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 var mypageRouter = require('./routes/mypage');
+var boardRouter = require('./routes/board');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
@@ -16,9 +18,15 @@ app.use(session({
     store: new FileStore()
 }));
 
+app.use(express.static(path.join(__dirname, './public')));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/mypage', mypageRouter);
+app.use('/board', boardRouter);
 
 // app.use(function(req, res, next) {
 //     res.status(404).send('Sorry cant find that!');
